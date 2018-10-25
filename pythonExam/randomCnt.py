c6684688bb05ac8e4438fc9ebd2340c796c1687a
chlehdrl <ace010817@gmail.com>
2018-10-25 16:04:50 +0900

    rename for python

--- pythonExam/randomCnt.py
+++ pythonExam/randomCnt.py
@@ -1,19 +1,26 @@
-mport random
+import random
 rn = random.randrange(1,1001)
 num = 0
 t_cnt = 0
 
-print("game start")
 
-while (rn !=num)
+print('Game Start')
 
-    num = int(input("Enter a number between 1 and 1000")
+
+while(rn !=num)
+
+
+    num = int(input("Enter a number between 1 and 1001"))
+
 
     if (num > rn):
 	print("Down")
     elif (num < rn):
 	print("UP")
 
+
     t_cnt+=1
 
-print("GAME SET")
+
+
+print("Game Set")
